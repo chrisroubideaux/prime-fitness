08cd3899b292c1380eaedc51d97ad2ce33d60d2f
chris roubideaux <51684535+chrisroubideaux@users.noreply.github.com>
2025-02-12 21:03:12 -0600

    updated navbar

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack(config, { dev }) {
+    if (!dev) {
+      // Disable CSS minification in production
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
